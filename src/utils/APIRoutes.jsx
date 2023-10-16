ebdd154c0805f7696dc1f178f73b27eb4f9cda89
shweta <91779835+ShwetaPandey0407@users.noreply.github.com>
2023-10-16 16:03:08 +0530

    2st commit

--- src/utils/APIRoutes.jsx
+++ src/utils/APIRoutes.jsx
@@ -1,5 +1,6 @@
 // export const host = "http://localhost:5000";
- 
+
+export const host = "https://backend-chat-app-joiz.onrender.com";
 
 export const registerRoute =`${host}/api/auth/register`;
 export const loginRoute =`${host}/api/auth/login`;
